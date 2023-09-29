f81b1a530869aebaa5ef79daa5d0cc63a23c7bc6
Kazutoo12 <kirigaya120897@gmail.com>
2023-09-29 17:35:59 +0700

    Update Responsive

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,6 @@
 import React from "react";
 import Hero from "../components/Hero";
 import Search from "../components/Search";
-import PopularFrined from "../components/PopularFrined";
-import BeginnerFriend from "../components/BeginnerFriend";
-import PopularArticle from "../components/PopularArticle";
-import KategoriBelajar from "../components/KategoriBelajar";
 import Footer from "../components/Footer";
 
 const Home = () => {
@@ -12,10 +8,6 @@ const Home = () => {
     <>
       <Hero />
       <Search />
-      <PopularFrined />
-      <BeginnerFriend />
-      <PopularArticle />
-      <KategoriBelajar />
       <Footer />
     </>
   );
