7f9f71b28418d0ea6dedd95907739595e3bb5be0
Kazutoo12 <kirigaya120897@gmail.com>
2023-09-29 17:32:21 +0700

    Update Responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import MainLogo from "./molecules/MainLogo";
 import NavList from "./molecules/NavList";
 import NavMobile from "./molecules/NavMobile";
