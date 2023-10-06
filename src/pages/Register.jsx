d43b92a3c3f30cdb27ddff9c0c4676b8222eb27f
Kazutoo12 <kirigaya120897@gmail.com>
2023-10-06 23:04:18 +0700

    Update Validasi

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -166,9 +166,9 @@ const Register = () => {
                         },
                         pattern: {
                           value:
-                            /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/,
+                            /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*]).{8,}$/,
                           message:
-                            "Password must be at least 8 characters long and include at least 1 uppercase letter, 1 lowercase letter, and 1 number",
+                            "Password must be at least 8 characters long and include at least 1 uppercase letter, 1 lowercase letter, 1 number, and 1 special character (!@#$%^&*)",
                         },
                       })}
                     />
